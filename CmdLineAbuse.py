aa6e10ee600a3390a162e6b1127ac159832ee9a3
Zach Kirkland <zachboy82@gmail.com>
2013-07-08 14:15:49 -0500

    Updated commands and added a default "random" command.

--- CmdLineAbuse.py
+++ CmdLineAbuse.py
@@ -1,28 +1,47 @@
+from random import randint
+
+abuseList = ["\nYou smack yourself hard on the forehead.",
+            "\nYou punch yourself in the stomach.",
+            "\nYou poke yourself in the eye.",
+            "\nYou pinch yourself on the leg.",
+            "\nYou hold your breath until you pass out."]
+
+def abuseRandom():
+    print(abuseList[randint(0, 4)])
+
+def outPinch():
+    print(abuseList[3])
+
+def outHold():
+    print(abuseList[4])
+
 def outHit():
-    print("\nYou smack yourself on the forehead!")
+    print(abuseList[0])
     
 def outPunch():
-    print("\nYou punch yourself in the stomach!")
+    print(abuseList[1])
 
 def outPoke():
-    print("\nYou poke yourself in the eye!")
+    print(abuseList[2])
 
-funcOutDict = {"hit" : outHit, "punch" : outPunch, "poke" : outPoke}
+funcOutDict = {"hit" : outHit, "punch" : outPunch, "poke" : outPoke,
+                "pinch" : outPinch, "" : abuseRandom}
 
 
 def main():
     print("The self-abuse helper command line interface...\n"
-        "Type q to quit.\n")
+        "Type q to quit or press enter for a random abuse.\n")
     print("You are depressed and self abusive.\n")
     cont = True
     while(cont):
-        selection = input('\nDo you want to hit, punch, or poke?: ')
+        selection = input('\nDo you want to hit, punch, poke, or pinch?: ')
         if selection == 'q': # quick and dirty way to give the user a way out
             print("\nYou feel slightly better after you regain conciousness.\n")
             cont = False
         elif selection in funcOutDict.keys():
             funcOutDict[selection]()
         else:
-            print ('Enter q to quit.')
+            print ('\nThat\'s where I draw the line buddy! Enter q to quit'
+                    ' or press enter for a random abuse.')
 
 main()
